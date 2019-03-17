dda799b99f2d3693fd4193be816e390276efc556
Seunghan Lim <teoac81@gmail.com>
2019-03-18 01:54:23 +0900

    Add convt_udacity_steering.py file.

---
+++ utils/convt_udacity_steering.py
@@ -0,0 +1,49 @@
+import csv
+import shutil
+
+csv_open = open('final_example.csv', 'r')
+csv_reader = csv.reader(csv_open)
+header = next(csv_reader) # header 읽고 다음행으로 이동
+for row in csv_reader:
+    fNo = row[0]
+    sAng = float(row[1])
+    print('Frame No:', fNo, 'Steering Angle:', sAng)
+    tmp = sAng*10
+    tmp2 = round(tmp)
+    if tmp2 == -4:
+        tmp3 = 'Copy '+fNo+'.jpg to /m04'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'm04')
+    elif tmp2 == -3:
+        tmp3 = 'Copy '+fNo+'.jpg to /m03'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'm03')
+    elif tmp2 == -2:
+        tmp3 = 'Copy '+fNo+'.jpg to /m02'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'm02')
+    elif tmp2 == -1:
+        tmp3 = 'Copy '+fNo+'.jpg to /m01'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'm01')
+    elif tmp2 == 1:
+        tmp3 = 'Copy '+fNo+'.jpg to /p01'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'p01')
+    elif tmp2 == 2:
+        tmp3 = 'Copy '+fNo+'.jpg to /p02'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'p02')
+    elif tmp2 == 3:
+        tmp3 = 'Copy '+fNo+'.jpg to /p03'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'p03')
+    elif tmp2 == 4:
+        tmp3 = 'Copy '+fNo+'.jpg to /p04'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', 'p04')
+    else:
+        tmp3 = 'Copy '+fNo+'.jpg to /000'
+        print(tmp3)
+        shutil.copy('./center/'+fNo+'.jpg', '000')
+csv_open.close()
--- wiki_docs/DIGITS_UDACITY_DB.md
+++ wiki_docs/DIGITS_UDACITY_DB.md
@@ -1,1 +1,14 @@
-test
+* [Udacity dataset 다운로드](https://github.com/udacity/self-driving-car/blob/master/datasets/CH2/Ch2_001.tar.gz.torrent)
+  * dataset을 다운로드 할 수 있는 torrent 파일이 공유되어 있다.
+  * 이 파일을 실행시키면 우분투의 Transmission BitTorrent Client를 통해 원본파일을 다운로드할 수 있다.
+
+
+* [Collision dataset 다운로드](http://rpg.ifi.uzh.ch/data/collision.zip)
+
+* 두 파일을 적절하게 압출을 풀어둔다.
+  * 예) Udacity dataset: `~/data/data_Udacity/driving`
+  * 예) Collision dataset: `~/data/data_Udacity/collision`
+
+
+* Udacity dataset 압축을 푼 곳에 `/center` 디렉토리가 있는지 확인한다.
+* `/center` 디렉토리가 있는 곳에 [convt_udacity_steering.py]()를 다운로드 받는다.
